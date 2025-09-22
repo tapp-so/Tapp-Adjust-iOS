851721bb4aa9bfeade462fdcb92fac56a6cbdfda
alex_stergiou <alex.stergiou@hotmail.com>
2025-09-22 15:15:47 +0100

    exposes typealiases

--- Sources/TappAdjust/Tapp+Adjust.swift
+++ Sources/TappAdjust/Tapp+Adjust.swift
@@ -1,7 +1,17 @@
 import AdjustSdk
 import TappCore
+import TappNetworking
 
 public typealias Tapp = TappCore
+public typealias Affiliate = TappNetworking.Affiliate
+public typealias AffiliateURLConfiguration = TappNetworking.AffiliateURLConfiguration
+public typealias DeferredLinkDelegate = TappNetworking.DeferredLinkDelegate
+public typealias EventActionMapper = TappNetworking.EventActionMapper
+public typealias EventAction = TappNetworking.EventAction
+public typealias EventConfig = TappNetworking.EventConfig
+public typealias TappError = TappNetworking.TappError
+public typealias TappConfiguration = TappNetworking.TappConfiguration
+public typealias Environment = TappNetworking.Environment
 
 extension Tapp {
 
